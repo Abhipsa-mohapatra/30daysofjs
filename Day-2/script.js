a8fbdbaf02324c52dfbac3114dd9361a75cef88e
Abhipsa Mohapatra <abhipsa.mohapatra@techriff.in>
2022-07-23 01:37:39 +0530

    d-2 js assignment complete

--- Day-2/script.js
+++ Day-2/script.js
@@ -95,3 +95,25 @@ let sentence = `"Love is the best thing in this world. Some found their love and
 console.log(sentence);
 //console.log(sentence.match("love"));
 console.log(sentence.match(/love/g)); //q-1
+
+console.log(str);
+console.log(str.match(/because/gi)); //q-2
+
+const Sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+console.log(Sentence); // print the original value
+console.log(Sentence.replace(/[&%@#;]/g, "")); //remove ONLY THE GIVEN characters
+console.log(Sentence.replace(/[^a-zA-Z ]/g, "")); //remove all the special characters
+
+string =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+let salarymonth = 5000;
+let salaryannual = salarymonth * 12;
+console.log(salaryannual);
+let annualbonus = 10000;
+console.log(annualbonus);
+let onlinemonth = 15000;
+let onlineannual = onlinemonth * 12;
+console.log(onlineannual);
+let annualincome = salaryannual + annualbonus + onlineannual;
+console.log(annualincome);
